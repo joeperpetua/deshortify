8870db5159d12990b46e599d81c8c6c0dc921ed9
joeperpetua <qwerty.perpetua@gmail.com>
2025-10-16 00:20:10 +0200

    refactor: run feed remover on mutation for better reliability

--- src/content.ts
+++ src/content.ts
@@ -6,26 +6,13 @@ import {
 } from "./removers";
 import { redirectOnShorts } from "./utils";
 
-let removedShortsOnLoad = false;
-
 const observer = new MutationObserver(async () => {
   redirectOnShorts();
   removeShortsFromSidebar();
   removeShortsFromChannelTab();
   removeShortsFromSuggestions();
-
-  // Only remove shorts drawer on mutation during the page load,
-  // after this is done we can rely on scroll only
-  if (!removedShortsOnLoad) {
-    const removed = await removeShortsFromFeed();
-    if (removed) removedShortsOnLoad = true;
-  }
-});
-
-// Looking for shorts drawer in feed is a bit more expensive, so we do it on scroll instead of mutation
-document.onscroll = () => {
   removeShortsFromFeed();
-};
+});
 
 observer.observe(document.body, {
   subtree: true,
